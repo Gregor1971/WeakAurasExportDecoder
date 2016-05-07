e07eab9e6cd300063551bd68887b38802adbf77b
Gregor71 <millergregor@gmail.com>
2016-05-07 19:02:11 +0000

    Using MoonSharp

---
+++ WeakAurasTimerBarCreator/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MoonSharp.Interpreter;
+
+namespace WeakAurasTimerBarCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string _wowRoot = @"C:\WoW";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindowLoaded;
+        }
+
+        private void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Script lua = new Script(CoreModules.Preset_Complete);
+
+            lua.DoFile(@"bit.lua");
+            lua.DoFile(@"WoWStub.lua");
+
+            lua.DoString("strsub, strsplit, strlower, strmatch, strtrim = string.sub, string.split, string.lower, string.match, string.trim");
+
+            lua.DoFile(_wowRoot + @"\Interface\AddOns\WeakAuras\libs\LibStub\LibStub.lua");
+            lua.DoFile(_wowRoot + @"\Interface\AddOns\WeakAuras\libs\LibCompress\LibCompress.lua");
+
+            lua.DoString("local f = load(\"local i = 5; i = i + 1\"); f()");
+
+            //lua.DoString("loadstring = load");
+            lua.DoFile(@"WeakAurasStub.lua");
+            //lua.DoFile(_wowRoot + @"\Interface\AddOns\WeakAuras\Transmission.lua");
+        }
+    }
+}
---
+++ WeakAurasTimerBarCreator/WeakAurasStub.lua
@@ -0,0 +1,43 @@
+﻿WeakAuras = {}
+
+local loadstring = load
+
+local myFunc = assert(loadstring("local i = 5; i = i + 1"))()
+
+local encode_func = assert(loadstring("return function(self, str) return str:gsub(self.encode_search, self.encode_translate); end"))()
+
+local Compresser = LibStub:GetLibrary("LibCompress");
+local Encoder = Compresser:GetAddonEncodeTable()
+--[[
+local Serializer = LibStub:GetLibrary("AceSerializer-3.0");
+
+
+function TableToString(inTable, forChat)
+    local serialized = Serializer:Serialize(inTable);
+    local compressed = Compresser:CompressHuffman(serialized);
+    if(forChat) then
+        return encodeB64(compressed);
+    else
+        return Encoder:Encode(compressed);
+    end
+end
+
+function StringToTable(inString, fromChat)
+    local decoded;
+    if(fromChat) then
+        decoded = decodeB64(inString);
+    else
+        decoded = Encoder:Decode(inString);
+    end
+    local decompressed, errorMsg = Compresser:Decompress(decoded);
+    if not(decompressed) then
+        return "Error decompressing: "..errorMsg;
+    end
+    local success, deserialized = Serializer:Deserialize(decompressed);
+    if not(success) then
+        return "Error deserializing "..deserialized;
+    end
+    return deserialized;
+end
+
+]]--
